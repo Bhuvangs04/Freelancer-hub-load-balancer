8119c9f6be3bd858edfdd8e2a6002398fbff43e4
Bhuvangs04 <bhuvangs2004@gmail.com>
2025-03-09 01:53:59 +0530

    70% completed
    Date 08-03-2025

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 const servers = [
-  { url: "http://localhost:3000", active: true },
-  { url: "http://localhost:3001", active: true },
+  { url: "https://freelancerhubbackend.vercel.app", active: true },
+  { url: "https://freelancer-hub-backend-five.vercel.app", active: true },
 ];
 
 module.exports = servers;
