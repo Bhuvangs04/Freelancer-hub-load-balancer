ab6483f6cea3329fed0f39f558e54df1d797d7f7
Bhuvan G Sangappanavar <bhuvangs2004@gmail.com>
2025-05-27 10:50:50 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 const servers = [
   { url: "https://freelancer-hub-backend.onrender.com", active: true },
-  { url: "https://freelancer-hub-backend-five.vercel.app", active: false },
+  { url: "https://onsite-backend-com.onrender.com", active: true },
+  {url:"https://backend-onsite-com-2.onrender.com",active:true}
 ];
 
 module.exports = servers;
